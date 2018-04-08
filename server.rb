92f7cdc3f975bb25c6d1f5db45ccdc290c10478e
donrestarone <shashikejayatunge@gmail.com>
2018-04-08 14:52:44 -0400

    create basic layout, including routes and pages

--- server.rb
+++ server.rb
@@ -5,6 +5,18 @@ get '/frank-says' do #this is the path to the local part of the URL
 	
 end
 
+get '/' do
+  redirect to('/home')
+end
+
 get '/home' do 
 	erb :index
+end 
+
+get '/gallery' do 
+	erb :gallery
+end 
+
+get '/about-me' do 
+	erb :about_me
 end 
