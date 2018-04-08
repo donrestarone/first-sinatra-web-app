a1478173f1c006a8e79f5003aa2e95af0d8424c7
donrestarone <shashikejayatunge@gmail.com>
2018-04-08 16:11:34 -0400

    finished this project, for now. needs anchor links added to favourites websites page

--- server.rb
+++ server.rb
@@ -22,3 +22,9 @@ get '/about-me' do
 	@interests = ['Cats', 'Automotive Engineering', 'Music', 'Films', 'Coffee']
 	erb :about_me
 end 
+
+get '/favourites' do 
+	@favs = ['www.stancenation.com', 'www.github.com', 'www.airbnb.com', 'www.porsche.ca', 'www.petrolicious.com', 'www.singervehicledesign.com']
+	@links = ['Stancenation', 'Github', 'AirBnb', 'Porsche Canada', 'petrolicious', 'Singer Vehicle Design']
+	erb :favourites
+end 
